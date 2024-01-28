a60fe8879e70185d826bea2ed566ea3a40d30524
Roen Ivan Ylagan <roenylagan@gmail.com>
2024-01-28 17:10:57 +0800

    initial set of changes

---
+++ src/main/java/org/jpmc/ticketbookingserver/TicketBookingServerApplication.java
@@ -0,0 +1,13 @@
+package org.jpmc.ticketbookingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketBookingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketBookingServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jpmc/ticketbookingserver/TicketBookingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jpmc.ticketbookingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketBookingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
